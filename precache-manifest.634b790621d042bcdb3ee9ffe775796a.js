e9da6b6afe79095066f46d35079d771eb53ecb5b
alberto <albertomassa.info@gmail.com>
2020-01-07 22:06:58 +0100

    Updates

---
+++ precache-manifest.634b790621d042bcdb3ee9ffe775796a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938fcf8e87ddb8324b30a68ca392ebab",
+    "url": "/react-wod-timer/index.html"
+  },
+  {
+    "revision": "8dbb709954bbeb83b334",
+    "url": "/react-wod-timer/static/css/2.213982f1.chunk.css"
+  },
+  {
+    "revision": "7e10cd1c2296b19d1978",
+    "url": "/react-wod-timer/static/css/main.c7bf93ad.chunk.css"
+  },
+  {
+    "revision": "8dbb709954bbeb83b334",
+    "url": "/react-wod-timer/static/js/2.fdee4cd2.chunk.js"
+  },
+  {
+    "revision": "fb08aec389ff1d0e3b63dd26ad5ac153",
+    "url": "/react-wod-timer/static/js/2.fdee4cd2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e10cd1c2296b19d1978",
+    "url": "/react-wod-timer/static/js/main.f4872334.chunk.js"
+  },
+  {
+    "revision": "4ad589c7ab6dd7c9c30f",
+    "url": "/react-wod-timer/static/js/runtime-main.700fff16.js"
+  },
+  {
+    "revision": "a6bec1c4ef2d2fd1bac5a6951f5f32a3",
+    "url": "/react-wod-timer/static/media/TimerFont.a6bec1c4.ttf"
+  },
+  {
+    "revision": "53022d6f376f99f0a0268a55327b19d7",
+    "url": "/react-wod-timer/static/media/background-photo-mobile.53022d6f.jpg"
+  },
+  {
+    "revision": "e7c71c8b3db1fdff7a0fe5ad11791ed5",
+    "url": "/react-wod-timer/static/media/background-photo.e7c71c8b.jpg"
+  },
+  {
+    "revision": "c5d5cada987a7ab193fb01c58593fa5c",
+    "url": "/react-wod-timer/static/media/beep.c5d5cada.mp3"
+  },
+  {
+    "revision": "eef56b309661a3cfb4d751674e4d6084",
+    "url": "/react-wod-timer/static/media/final_beep.eef56b30.mp3"
+  }
+]);
